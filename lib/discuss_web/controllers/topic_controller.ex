9d2bb515abd9b66c4a2a1fbee25131d6e0fcf8b8
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-06-02 19:22:42 +0300

    Added adding a post and checking for an empty line but does not output an error

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,8 +2,9 @@ defmodule DiscussWeb.TopicController do
     use DiscussWeb, :controller
 
     alias DiscussWeb.Topic
+    alias Discuss.Repo
 
-    def new(conn, params) do
+    def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
 
 
@@ -11,6 +12,12 @@ defmodule DiscussWeb.TopicController do
     end
 
     def create(conn, %{"topic" => topic}) do
-        
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            {:ok, post} -> IO.inspect(post)
+            {:error, changeset} -> 
+            render conn, "new.html", changeset: changeset
+        end
     end
 end
