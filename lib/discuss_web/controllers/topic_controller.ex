dae725e7da838e65abfa9c04f10bb024ff296e5a
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-06-03 16:36:30 +0300

    Added list topics in index and changed h2 to h5

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -21,8 +21,11 @@ defmodule DiscussWeb.TopicController do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Repo.insert(changeset) do
-            {:ok, post} -> IO.inspect(post)
-            {:error, changeset} -> IO.inspect(changeset)
+            {:ok, post} -> 
+                conn
+                |> put_flash(:info, "Topic Created")
+                |> redirect(to: Routes.topic_path(conn, :index))
+            {:error, changeset} -> 
             render conn, "new.html", changeset: changeset, errors: changeset
         end
     end
