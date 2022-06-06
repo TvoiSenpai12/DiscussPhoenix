4e0b60f5afd970d05929063d0d1a192f919a3dba
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-06-06 13:04:09 +0300

    Added function edit post

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -21,7 +21,7 @@ defmodule DiscussWeb.TopicController do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Repo.insert(changeset) do
-            {:ok, post} -> 
+            {:ok, _topic} -> 
                 conn
                 |> put_flash(:info, "Topic Created")
                 |> redirect(to: Routes.topic_path(conn, :index))
@@ -30,10 +30,24 @@ defmodule DiscussWeb.TopicController do
         end
     end
 
-    def edit(conn, %{"id" => topics_id}) do
-        topic = Repo.get(Topic, topics_id)
+    def edit(conn, %{"id" => topic_id}) do
+        topic = Repo.get(Topic, topic_id)
         changeset = Topic.changeset(topic)
 
         render conn, "edit.html", changeset: changeset, topic: topic
+    end
+
+    def update(conn, %{"id" => topic_id, "topic" => topic}) do
+        old_topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(old_topic, topic)
+
+        case Repo.update(changeset) do
+            {:ok, _topic} ->
+                conn
+                |> put_flash(:info, "Topic Updated")
+                |> redirect(to: Routes.topic_path(conn, :index))
+            {:error, changeset} ->
+                render conn, "edit.html", changeset: changeset, topic: old_topic
+        end
     end
 end
