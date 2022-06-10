2ab61c704762be0bf096a497d5d3e5cc5537f70f
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-06-10 19:03:16 +0300

    Added conclusion comments

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -6,14 +6,36 @@ socket.connect()
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("ok", resp => {
+      renderComments(resp.comments);
+    })
+    .receive("error", resp => {
+       console.log("Unable to join", resp) 
+    })
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
 
     channel.push('comment:add', {content: content});
   });
+}
+
+function renderComments(comments) {
+  if (document.querySelectorAll('.collection-item').length === 0) {
+    const commentsUL = document.querySelector(".collection");
+    for (let comment of comments) {
+      const li = document.createElement("li");
+ li.classList.add("collection-item");
+ li.innerText = comment.content;
+ commentsUL.append(li);
+    }
+  } else {
+    const li = document.createElement("li")
+ li.classList.add("collection-item")
+ li.innerText = comments[comments.length -1 ].content
+ document.querySelector('.collection').append(li)
+ document.querySelector('textarea').value = ""
+  }
 }
 
 window.createSocket = createSocket;
